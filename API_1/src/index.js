474716eb7a8bc7435ef0432d7f97515ba32f6374
Ismael <ismael.dantas@hotmail.com>
2024-02-29 11:28:01 -0300

    Instancia Axios

--- API_1/src/index.js
+++ API_1/src/index.js
@@ -3,11 +3,22 @@ const axios = require('axios')
 
 const app = express()
 
+const instanciaAxios = axios.create({
+  baseURL: 'http://localhost:3001'
+})
+
 app.use(express.json())
 
 app.get('/', async (req, res) => {
   //res.json('api1, porta 3000')
-  const resultadoAxios = await axios.get('http://localhost:3001/carros')
+  //const resultadoAxios = await axios.get('http://localhost:3001/carros')
+
+  const novoCarro = {
+    modelo: 'Fusca',
+    marca: 'VW'
+  }
+
+  const resultadoAxios = await instanciaAxios.post('/carros', novoCarro)
 
   res.json(resultadoAxios.data)
 })
--- API_2/src/index.js
+++ API_2/src/index.js
@@ -4,21 +4,31 @@ const app = express()
 
 app.use(express.json())
 
+const carros = [
+  {
+    modelo: 'S10',
+    marca: 'Chevrolet'
+  },
+  {
+    modelo: 'Celta',
+    marca: 'Chevrolet'
+  }
+]
+
 app.get('/', async (req, res) => {
   res.json('api2, porta 3001')
 })
 
 app.get('/carros', async (req, res) => {
-  res.json([
-    {
-      modelo: 'S10',
-      marca: 'Chevrolet'
-    },
-    {
-      modelo: 'Celta',
-      marca: 'Chevrolet'
-    }
-  ])
+  res.json(carros)
+})
+
+app.post('/carros', async (req, res) => {
+  const { modelo, marca } = req.body
+
+  carros.push({ modelo, marca })
+
+  res.json(carros)
 })
 
 app.listen(3001)
